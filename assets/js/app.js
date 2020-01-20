5a4e1307583f4b4aa85946056b25b4639d646033
Andreea <46901502+andreeaiosip@users.noreply.github.com>
2020-01-20 19:28:03 +0000

    Canvas included on the footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,16 +5,59 @@ navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
 
+// Canvas 
 
-const c = document.getElementById("myCanvas");
-const ctx = c.getContext("2d");
+// Text canvas
+var canvasText = document.getElementById("myCanvas");
+var ctx = canvasText.getContext("2d");
+
+ctx.fillText("Welcome to Brasov!", 0, 50);
+ctx.font = "50px Arial";
+
+// Gradient responsive
+var context = document.querySelector("canvas").getContext("2d");
 
 // Create gradient
-var grd = ctx.createLinearGradient(0,0,200,0);
-grd.addColorStop(0,"#2376ae "); 
-grd.addColorStop(1,"#c16ecf ");
+var grd = context.createLinearGradient(0, 0, 300, 0);
+grd.addColorStop(0, "green");
+grd.addColorStop(1, "grey");
 
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(10,10,150,80);
 
+
+var render = function() {
+
+  context.canvas.width = document.documentElement.clientWidth * 0.5;
+  context.canvas.height = document.documentElement.clientHeight * 0.5;
+
+  context.fillStyle = grd;
+  context.fillRect(2, 5, context.canvas.width, context.canvas.height);
+
+context.font='15px Arial';
+context.fillText('How are you today?',100,100);
+
+};
+
+window.addEventListener("resize", render);
+
+render();
+
+// Slideshow on Home page
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
