2dbfc44162a7c3c4c1267aadbb7580507883c473
Andreea <46901502+andreeaiosip@users.noreply.github.com>
2020-01-20 14:09:30 +0000

    Canvas gradient fill added to the footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,17 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"#2376ae "); 
+grd.addColorStop(1,"#c16ecf ");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,150,80);
+
