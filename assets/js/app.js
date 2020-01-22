3f5bb64929aadd61cdf122b180b0cce3f00725a4
Andreea <46901502+andreeaiosip@users.noreply.github.com>
2020-01-22 15:02:38 +0000

    Reset of slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,4 +70,32 @@ ctx.textAlign = "center";
 ctx.fillText("Make your stay a wonderful stay at Brasov!", canvas.width/2, canvas.height/2);
 
 
+// slider
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
